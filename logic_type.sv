ff1f9525bfa12f6b0d39b72e9b648149c8a325b1
Pakkiresh-DV <tonyluvsu3000@gmail.com>
2024-09-19 23:44:40 +0530

    logic_type.sv

--- logic_type.sv
+++ logic_type.sv
@@ -1,1 +1,12 @@
+module logic_data_type(input logic rst_h);
+  parameter cycle = 20;
+  logic q,qb,d,clk,rst_l;
+  initial begin
+    clk <= 0;
+    forever #(cycle/2) clk = ~clk;
+  end
+  assign rst_l = ~rst_h;
+  not n1(qb,q);
+  dff d1(q,d,clk,rst_l);
+endmodule
 
